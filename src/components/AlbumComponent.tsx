c9ded5c3554f6ff67229a8bd65a4fee3cf4842b5
Abhishek Rathore <arathore@ingenuitygaming.com>
2024-07-18 13:21:54 +0530

    Fixed refresh issue

--- src/components/AlbumComponent.tsx
+++ src/components/AlbumComponent.tsx
@@ -1,12 +1,14 @@
-import { FC, useEffect } from "react";
 import styled from "styled-components";
-import { useSelector } from "react-redux";
+import { NotFound } from "./NotFound";
 import type { RootState } from "../store";
 import { imageData } from "../interfaces";
+import { getData } from "../communicator";
+import { FC, memo, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { TextComponent } from "./TextComponent";
 import { ImageComponent } from "./ImageComponenet";
 import { ButtonComponent } from "./ButtonComponent";
+import { useDispatch, useSelector } from "react-redux";
 
 const Wrapper = styled.section`
     display: flex;
@@ -15,23 +17,33 @@ const Wrapper = styled.section`
     justify-content: center;
 `;
 
-export const AlbumComponent: FC = () => {
-    const { albumId } = useParams();
-    const imgData: imageData[] = useSelector((state: RootState) => state.albumData.data);
-    useEffect(() => { }, [albumId]);
 
+const Album: FC<{ imgData: imageData, albumId: number }> = memo(({ imgData, albumId }) => {
     return (
         <Wrapper>
-            <ImageComponent key={`${albumId}-image`} src={imgData[Number(albumId) - 1].url} alt={`${albumId}-image`} />
+            <ImageComponent key={`${albumId}-image`} src={imgData.url} alt={`${albumId}-image`} />
             <TextComponent key={`${albumId}-Title`}>
                 <span><b>Title: </b></span>
-                <span>{`${imgData[Number(albumId) - 1].title}`}</span>
+                <span>{`${imgData.title}`}</span>
             </ TextComponent>
             <TextComponent key={`${albumId}-Album`}>
                 <span><b>Album ID: </b></span>
-                <span>{`${imgData[Number(albumId) - 1].albumId}`}</span>
+                <span>{`${imgData.albumId}`}</span>
             </ TextComponent>
             <ButtonComponent key={`${albumId}-btn`}>BACK</ButtonComponent>
         </Wrapper>
     );
+});
+
+export const AlbumComponent: FC = () => {
+    const dispatch = useDispatch();
+    const { albumId } = useParams();
+    const arrImgData: imageData[] = useSelector((state: RootState) => state.albumData.data);
+    const imgData: imageData = arrImgData && arrImgData[Number(albumId) - 1];
+
+    useEffect(() => {
+        arrImgData.length === 0 && getData(dispatch); //Retrive the data if the page was refreshed
+    }, [albumId, dispatch, arrImgData]);
+
+    return imgData ? <Album imgData={imgData} albumId={Number(albumId)} /> : <NotFound />
 }
